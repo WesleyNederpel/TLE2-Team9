3045e9ca99be6376c322027692829e62eec24efc
Hidde-Scheringa <hescheringa@gmail.com>
2025-06-16 10:49:17 +0200

    styling aangepast

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -147,7 +147,6 @@ const styles = StyleSheet.create({
     },
     modalOverlay: {
         flex: 1,
-        backgroundColor: 'rgba(0, 0, 0, 0.4)',
         justifyContent: 'center',
         alignItems: 'center',
     },
